97b5519c41eafac525323b21c93fdde0c36cbe68
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-04-04 16:07:41 +0200

    update gwasstudio version

--- vault-init/vault_init.py
+++ vault-init/vault_init.py
@@ -35,7 +35,7 @@ keys_to_store = {
 
 for key, value in keys_to_store.items():
     # Store the keys in Vault
-    store_path = f"secret/{key}"
+    store_path = f"{key}"
     client.secrets.kv.v2.create_or_update_secret(
         path=store_path,
         secret=value,
